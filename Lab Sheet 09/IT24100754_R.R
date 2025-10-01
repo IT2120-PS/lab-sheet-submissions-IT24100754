c7cf213540f3c262cb0d1ef404b7dfe864cbf180
IT24100754 <IT24100754@my.sliit.lk>
2025-10-01 20:42:52 +0530

    IT24100754

---
+++ Lab Sheet 09/IT24100754_R.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\User\\Desktop\\IT24100754")
+
+# Exercise 1
+# Part i
+
+baking_times <- rnorm(25, mean = 45, sd = 2)
+print(baking_times)
+
+# Part ii
+test_result <- t.test(baking_times, mu = 46, alternative = "less")
+
+print(test_result)
